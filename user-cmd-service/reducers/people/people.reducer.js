fecb03dfbc29e34fc7e213b0e0be38e0068451fe
James McLaughlin <James_McLaughlin@ultimatesoftware.com>
2016-06-22 17:22:56 -0400

    fixed people reducer in user command service

--- user-cmd-service/reducers/people/people.reducer.js
+++ user-cmd-service/reducers/people/people.reducer.js
@@ -1,12 +1,12 @@
 const _ = require('lodash');
 const {
-	PEOPLE_CREATED
+	PERSON_CREATED
 } = require('../../commands/event_types');
 
 function reducer(people = [], action ) {
 	console.log("In people reducer - ", action);
 	switch(action.type) {
-		case PEOPLE_CREATED:
+		case PERSON_CREATED:
 			return personCreated(people, action.payload);
 	}
 	return people;
