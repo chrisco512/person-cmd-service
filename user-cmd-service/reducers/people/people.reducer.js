1dd702dda972b91b2d4b9e2abb53d8e849aff127
James McLaughlin <James_McLaughlin@ultimatesoftware.com>
2016-06-22 09:19:32 -0400

    removed seed state in people reducer

--- user-cmd-service/reducers/people/people.reducer.js
+++ user-cmd-service/reducers/people/people.reducer.js
@@ -3,20 +3,16 @@ const {
 	PEOPLE_CREATED
 } = require('../../commands/event_types');
 
-const seedState = [{
-	_id: '28f0e756-0304-492e-9418-f51dd3c64700'
-}];
-
-function reducer(people = seedState, action ) {
+function reducer(people = [], action ) {
 	console.log("In people reducer - ", action);
 	switch(action.type) {
 		case PEOPLE_CREATED:
-			return tenantCreated(people, action.payload);
+			return personCreated(people, action.payload);
 	}
 	return people;
 }
 
-function tenantCreated(people, { _id }) {
+function personCreated(people, { _id }) {
 	return [...people, { _id }];
 }
 
