9b4fd4745b01ba8d5e7530761867b255ad2730e6
Chris Co <ufcordle@hotmail.com>
2016-07-06 00:53:14 -0400

    adding tests to pillar service

--- pillar-cmd-service/src/commands/pillar_create/index.js
+++ pillar-cmd-service/src/commands/pillar_create/index.js
@@ -1,5 +1,5 @@
-const validateCommand = require('./pillar_create.cmd.validator.js');
-const createEvent = require('./pillar_created.event.creator.js');
+const validateCommand = require('./pillar_create.cmd.validator');
+const createEvent = require('./pillar_created.event.creator');
 const dispatchEvent = require('../../common/dispatch_event.chainable');
 const persistEvent = require('../../common/persist_event.chainable');
 const publishEvent = require('../../common/publish_event.chainable');
