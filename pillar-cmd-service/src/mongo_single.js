575432a5ceaffe27c7d95e8d8d445dee32586407
Ben Botwin <thecodethinker@gmail.com>
2016-07-13 16:05:04 -0400

    added mongo safety to pillar service

---
+++ pillar-cmd-service/src/mongo_single.js
@@ -0,0 +1,28 @@
+const MongoClient = require('mongodb').MongoClient;
+const config = require('./config');
+const { uri } = config.mongo;
+
+let dbInstance;
+
+function connect() {
+	return new Promise((resolve, reject) => {
+		if (dbInstance !== undefined) {
+			resolve(dbInstance);
+		}
+
+		MongoClient.connect(uri, function (err, db) {
+			if(err) {
+				reject(err);
+			}
+
+			if(db) {
+				dbInstance = db;
+				resolve(dbInstance);
+			}
+		});
+	});
+}
+
+module.exports = {
+	connect
+};
