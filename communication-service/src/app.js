61372efc2ada8e3aad19c46471afbe91cf041e3b
Ben Botwin <thecodethinker@gmail.com>
2016-07-14 18:07:53 -0400

    added test email

---
+++ communication-service/src/app.js
@@ -0,0 +1,35 @@
+const config = require('./config');
+const bus = require('servicebus').bus({ url: config.servicebus.uri + '?heartbeat=60' });
+
+let transporter = nodemailer.createTransport(ses({
+        accessKeyId: 'AKIAINFX7ENSQB3GYXYA',
+        secretAccessKey: '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',
+        region: 'us-west-2'
+}));
+
+bus.subscribe('#', (event) => {
+  	console.log('SOCKET SERVICE RECEIVED: ', event);
+
+        if(event.type === "person.PERSON_CREATED") {
+                sendEmail(event.payload)
+        }
+});
+
+
+function sendEmail(payload) {
+        console.log(`sending mail to, ${payload.firstName} ${payload.lastName}`);
+
+        transporter.sendMail({
+                from: 'culture@culture.xyz',
+                to: payload.email,
+                subject: "Welcome to CultureShock!",
+                html: "You have been assimilated"
+        }, function(error, info) {
+                if (error) {
+                        console.log(`OH NOES! ERROR: ${error}`);
+                        return;
+                }
+
+                console.log('Message sent: ', info);
+        });
+}
