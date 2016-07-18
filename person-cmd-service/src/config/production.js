8520a477ee7af3e0bc6c8c6b4df00e32845f2b33
Ben Diuguid <bendiuguid@gmail.com>
2016-07-18 18:19:49 -0400

    lint person-cmd

--- person-cmd-service/src/config/production.js
+++ person-cmd-service/src/config/production.js
@@ -1,14 +1,14 @@
 'use strict';
-var path = require('path');
+const path = require('path');
 
 // PRODUCTION
 module.exports = {
   port: process.env.PORT || 80,
   servicebus: {
-      uri: process.env.RABBIT_URI || "amqp://rabbit"
+      uri: process.env.RABBIT_URI || 'amqp://rabbit'
   },
   mongo: {
-      uri: process.env.MONGO_URI || "mongodb://mongo/culture"
+      uri: process.env.MONGO_URI || 'mongodb://mongo/culture'
   },
   root: path.normalize(`${__dirname}/../../..`), // Root path of server
   secrets: {
