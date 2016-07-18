374d70c671dec5a43a9abf7310cb4088b78b1d08
James McLaughlin <james_mclaughlin@ultimatesoftware.com>
2016-07-18 15:30:11 -0400

    refactored mutations folder to use subfolders

--- query-service/src/schema/mutations/index.js
+++ query-service/src/schema/mutations/index.js
@@ -1,6 +1,24 @@
+const PERSON = require('./person');
+const USER = require('./user');
+const TENANT = require('./tenant');
+const SAGA = require('./saga');
+
 module.exports = {
-  USER_CREATE: require('./user_create'),
-  PERSON_CREATE: require('./person_create'),
-  TENANT_CREATE: require('./tenant_create'),
-  EMPLOYEE_IMPORT_SAGA: require('./employee_import_saga')
+  USER_CREATE: USER.CREATE,
+  PERSON_CREATE: PERSON.CREATE,
+  PERSON_FIRST_NAME_CHANGE: PERSON.FIRST_NAME_CHANGE,
+  PERSON_LAST_NAME_CHANGE: PERSON.LAST_NAME_CHANGE,
+  PERSON_EMAIL_CHANGE: PERSON.EMAIL_CHANGE,
+  PERSON_PHONE_CHANGE: PERSON.PHONE_CHANGE,
+  PERSON_CARRIER_CHANGE: PERSON.CARRIER_CHANGE,
+  TENANT_CREATE: TENANT.CREATE,
+  EMPLOYEE_IMPORT_SAGA: SAGA.EMPLOYEE_IMPORT,
 };
+
+
+// module.exports = {
+//   USER_CREATE: require('./user_create'),
+//   PERSON_CREATE: require('./person_create'),
+//   TENANT_CREATE: require('./tenant_create'),
+//   EMPLOYEE_IMPORT_SAGA: require('./employee_import_saga'),
+// };
---
+++ query-service/src/schema/mutations/person/index.js
@@ -0,0 +1,15 @@
+const CREATE = require('./person_create.js');
+const FIRST_NAME_CHANGE = require('./person_first_name_change');
+const LAST_NAME_CHANGE = require('./person_first_name_change');
+const EMAIL_CHANGE = require('./person_email_change');
+const PHONE_CHANGE = require('./person_phone_change');
+const CARRIER_CHANGE = require('./person_carrier_change');
+
+module.exports = {
+  CREATE,
+  FIRST_NAME_CHANGE,
+  LAST_NAME_CHANGE,
+  EMAIL_CHANGE,
+  PHONE_CHANGE,
+  CARRIER_CHANGE
+};
