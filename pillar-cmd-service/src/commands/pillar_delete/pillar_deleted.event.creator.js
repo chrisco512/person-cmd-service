f4c654b046b16f30e734e3d770cd4425b34fd150
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2016-07-19 12:30:38 -0400

    deleted a comment

--- pillar-cmd-service/src/commands/pillar_delete/pillar_deleted.event.creator.js
+++ pillar-cmd-service/src/commands/pillar_delete/pillar_deleted.event.creator.js
@@ -2,8 +2,6 @@ const { PILLAR_DELETED } = require('../event_types');
 const log = require('../../log');
 
 function pillarRemoved({ _id, tenantId, name, content, isDeleted }) {
-	// isDeleted = true;
-
 	log.info(PILLAR_DELETED, ' : ', { _id, tenantId, name, content, isDeleted });
 	return Promise.resolve({
 		type: PILLAR_DELETED,
