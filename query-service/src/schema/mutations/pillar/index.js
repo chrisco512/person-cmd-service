92b2cf13c783a93c563ce366747fc35141f451cd
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2016-07-25 14:35:38 -0400

    pillar mutations yay

---
+++ query-service/src/schema/mutations/pillar/index.js
@@ -0,0 +1,9 @@
+const CREATE = require('./pillar_create');
+const DELETE = require('./pillar_delete');
+const NAME_CHANGE = require('./pillar_name_change');
+
+module.exports = {
+  CREATE,
+  DELETE,
+  NAME_CHANGE
+};
