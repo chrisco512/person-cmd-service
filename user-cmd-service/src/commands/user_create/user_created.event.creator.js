ae8b80369e412d74822f06158177194df81b9bbc
Ben Diuguid <bendiuguid@gmail.com>
2016-07-26 12:11:13 -0400

    added seed script

--- user-cmd-service/src/commands/user_create/user_created.event.creator.js
+++ user-cmd-service/src/commands/user_create/user_created.event.creator.js
@@ -1,14 +1,31 @@
-const { USER_CREATED } = require('../event_types');
+const {
+  USER_CREATED
+} = require('../event_types');
 const uuid = require('node-uuid');
 
 function userCreated({ role, tenantId, auth0Id, personId, companyIdentifier, email }) {
-	                                        console.log('EVENT CREATOR:', { role, tenantId, auth0Id, personId, companyIdentifier, email });
-	                                        const _id = uuid.v4();
+  console.log('EVENT CREATOR:', {
+    role,
+    tenantId,
+    auth0Id,
+    personId,
+    companyIdentifier,
+    email
+  });
+  const _id = uuid.v4();
 
-	                                        return {
-		                                        type: USER_CREATED,
-		                                        payload: { _id, role, tenantId, auth0Id, personId, companyIdentifier, email }
-	};
+  return {
+    type: USER_CREATED,
+    payload: {
+      _id,
+      role,
+      tenantId,
+      auth0Id,
+      personId,
+      companyIdentifier,
+      email
+    }
+  };
 }
 
 module.exports = userCreated;
