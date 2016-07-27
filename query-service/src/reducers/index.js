39cf5aaab0b36c98d01ded05df2e9c2857faded3
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2016-07-27 09:27:08 -0400

    added content created and deleted mutations, reducers, and types

--- query-service/src/reducers/index.js
+++ query-service/src/reducers/index.js
@@ -5,13 +5,15 @@ const employees = require('./employees/employees.reducer.js');
 const persons = require('./persons/person.reducer.js');
 const pillars = require('./pillars/pillar.reducer.js');
 const users = require('./users/user.reducer.js');
+const contents = require('./contents/contents.reducer.js');
 
 const reducers = {
 	tenants,
 	employees,
 	persons,
-        pillars, 
-        users
+  pillars,
+  users,
+	contents
 };
 
 module.exports = combineReducers(reducers);
---
+++ query-service/src/schema/mutations/content/index.js
@@ -0,0 +1,7 @@
+const CREATE = require('./content_create');
+const DELETE = require('./content_delete');
+
+module.exports = {
+  CREATE,
+  DELETE
+};
